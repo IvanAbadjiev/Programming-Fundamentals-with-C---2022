55aacede0cab947d0d4478cab06d1b42a94c1860
IvanAbadjiev <ivan087611@gmail.com>
2022-09-23 12:57:33 +0300

    More Exercise

---
+++ Basic Syntax, Conditional Statements and Loops/More Exercise/P04. Reverse String/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P04._Reverse_String
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read input 
+            string word = Console.ReadLine();
+            //verable to save reverse string
+            string reversWord = string.Empty;
+
+            //for loop to reverse string 
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                char currentCh = word[i];
+                reversWord += currentCh;
+
+            }
+
+            //print reverse string
+            Console.WriteLine(reversWord);
+        }
+    }
+}
