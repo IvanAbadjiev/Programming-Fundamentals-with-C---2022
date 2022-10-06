a5eaa14b5e45b4147f6960a87ae140a6c8cb9b43
IvanAbadjiev <ivan087611@gmail.com>
2022-10-06 11:09:35 +0300

    Arrays
    • What are Arrays?;
    • Reading Arrays from the Console;
    • Foreach Loop.

---
+++ Arrays/Lab/P01. Day of Week/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace P01._Day_of_Week
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read number represent day of week
+            int number = int.Parse(Console.ReadLine());
+
+            //read array day data 
+            string[] daysOfWeek = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+
+            //switch to choice day
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine(daysOfWeek[0]);
+                    break;
+                case 2:
+                    Console.WriteLine(daysOfWeek[1]);
+                    break;
+                case 3:
+                    Console.WriteLine(daysOfWeek[2]);
+                    break;
+                case 4:
+                    Console.WriteLine(daysOfWeek[3]);
+                    break;
+                case 5:
+                    Console.WriteLine(daysOfWeek[4]);
+                    break;
+                case 6:
+                    Console.WriteLine(daysOfWeek[5]);
+                    break;
+                case 7:
+                    Console.WriteLine(daysOfWeek[6]);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid day!");
+                    break;
+            }
+
+
+        }
+    }
+}
