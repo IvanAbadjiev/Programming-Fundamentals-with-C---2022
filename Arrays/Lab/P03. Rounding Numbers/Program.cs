b72960183e0b6aecb0c9568f5dffcb89e03c1264
IvanAbadjiev <ivan087611@gmail.com>
2022-10-06 11:15:52 +0300

    Arrays
    • What are Arrays?;
    • Reading Arrays from the Console;
    • Foreach Loop.

---
+++ Arrays/Lab/P03. Rounding Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P03._Rounding_Numbers
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //read an array of real numbers
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            //save celing data
+            int[] celing = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                celing[i] = (int)Math.Ceiling(numbers[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {celing[i]}");
+            }
+
+
+
+        }
+    }
+}
