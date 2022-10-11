138c46fa3e3ae141b3fe9fee687525aba6d79f07
IvanAbadjiev <ivan087611@gmail.com>
2022-10-11 15:25:22 +0300

    Exercise

---
+++ Arrays/Exercise/P01. Train/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P01._Train
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // n -> number of wagons
+            int n = int.Parse(Console.ReadLine());
+            //save data 
+            int sum = 0;
+            int[] peopleInwagons = new int[n];
+
+            //wagine people
+            for (int i = 0; i < n; i++)
+            {
+                int curr = int.Parse(Console.ReadLine());
+                peopleInwagons[i] = curr;
+                sum += curr;
+            }
+            Console.WriteLine(string.Join(" ", peopleInwagons));
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
---
+++ Arrays/Exercise/P02. Common Elements/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace P02._Common_Elements
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //read two arr
+            string[] firstElement = Console.ReadLine()
+                .Split();
+
+            string[] secondElement = Console.ReadLine()
+                .Split();
+            //save item
+            //string[] equal = new string[];
+
+            //comparison of the two items
+            for (int i = 0; i < secondElement.Length; i++)
+            {
+                string elementOne = secondElement[i];
+                for (int j = 0; j < firstElement.Length; j++)
+                {
+                    if (elementOne == firstElement[j])
+                    {
+
+                        Console.Write(secondElement[i] + " ");
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
