6f2d5c418a430f375cb2a8a7669790624b493293
IvanAbadjiev <ivan087611@gmail.com>
2022-10-11 15:27:31 +0300

    Exercise

---
+++ Arrays/Exercise/P05. Top Integers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace P05._Top_Integers
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTopInteger = true;
+                int currNum = numbers[i];
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int nextNum = numbers[j];
+                    if (nextNum >= currNum)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+
+                }
+                if (isTopInteger)
+                {
+                    Console.Write(currNum + " ");
+                }
+
+
+
+            }
+
+        }
+    }
+}
---
+++ Arrays/Exercise/P06. Equal Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace P06._Equal_Sum
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isEqual = false;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                int leftSum = 0;
+                int rigthSum = 0;
+                for (int j = 0; j <= i; j++)
+                {
+                    leftSum += numbers[j];
+                }
+                for (int k = i; k <= numbers.Length - 1; k++)
+                {
+                    rigthSum += numbers[k];
+                }
+                if (leftSum == rigthSum)
+                {
+                    Console.WriteLine(i);
+                    isEqual = true;
+                }
+
+
+
+            }
+            if (!isEqual)
+            {
+                Console.WriteLine("no");
+            }
+
+
+        }
+    }
+}
