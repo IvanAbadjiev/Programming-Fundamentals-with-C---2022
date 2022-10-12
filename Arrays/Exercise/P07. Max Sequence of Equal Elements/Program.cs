200bd93aa13c94f64362520912e880fe0724895f
IvanAbadjiev <ivan087611@gmail.com>
2022-10-12 23:09:21 +0300

    Exercise

---
+++ Arrays/Exercise/P07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace P07._Max_Sequence_of_Equal_Elements
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] numbers = Console.ReadLine()
+              .Split()
+              .Select(int.Parse)
+              .ToArray();
+
+            int sequence = 0;
+            int start = 0;
+            int end = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currNums = numbers[i];
+                int thisSequence = 0;
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    int nextNum = numbers[j];
+                    if (currNums == nextNum)
+                    {
+                        thisSequence++;
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    if (thisSequence > sequence)
+                    {
+
+                        start = i;
+                        end = j;
+                        sequence = thisSequence;
+                    }
+                }
+
+            }
+            //Console.WriteLine(start);
+            //Console.WriteLine(end);
+            //Console.WriteLine(sequence);
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ Arrays/Exercise/P08. Magic Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P08._Magic_Sum
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currNum = numbers[i];
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int nexNum = numbers[j];
+                    if ((currNum + nexNum) == magicNumber)
+                    {
+                        Console.WriteLine($"{currNum} {nexNum}");
+                    }
+
+                }
+
+
+            }
+
+
+        }
+    }
+}
