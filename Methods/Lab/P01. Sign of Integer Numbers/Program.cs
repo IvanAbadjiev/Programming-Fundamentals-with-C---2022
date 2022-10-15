b8b12bbf1eec15d24c6064b8c2605c2612547918
IvanAbadjiev <ivan087611@gmail.com>
2022-10-15 22:28:48 +0300

    Methods
    • Declaring and Invoking Methods;
    • Value vs Reference Types;
    • Overloading Methods;
    • Naming and Best Practices.

---
+++ Methods/Lab/P01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input number 
+            int number = int.Parse(Console.ReadLine());
+
+            ChecksNumberPositiveNegativeOrZero(number);
+        }
+        static void ChecksNumberPositiveNegativeOrZero(int num)
+        {
+            if (num < 0)
+            {
+                Console.WriteLine($"The number {num} is negative. ");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive. ");
+            }
+            else
+            {
+                Console.WriteLine($"The number {num} is zero. "
+);
+            }
+        }
+    }
+}
---
+++ Methods/Lab/P02. Grades/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace P02._Grades
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //receves grade 
+            double grade = double.Parse(Console.ReadLine());
+
+            string printGradeLevel = ChecksGradeLevel(grade);
+
+            Console.WriteLine(printGradeLevel);
+
+        }
+
+        static string ChecksGradeLevel(double grade)
+        {
+            string gradeLevel = string.Empty;
+            if (grade >= 2.00 && grade <= 2.99)
+            {
+                gradeLevel = "Fail";
+            }
+            else if (grade >= 3.00 && grade <= 3.49)
+            {
+                gradeLevel = "Poor";
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                gradeLevel = "Good";
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                gradeLevel = "Very good";
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                gradeLevel = "Excellent";
+            }
+
+            return gradeLevel;
+
+
+        }
+    }
+}
