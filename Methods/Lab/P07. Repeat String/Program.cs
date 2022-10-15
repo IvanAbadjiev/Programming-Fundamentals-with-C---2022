4582edff4df84d3c8f951320468127539d77039c
IvanAbadjiev <ivan087611@gmail.com>
2022-10-15 22:32:11 +0300

    Methods
    • Declaring and Invoking Methods;
    • Value vs Reference Types;
    • Overloading Methods;
    • Naming and Best Practices.

---
+++ Methods/Lab/P07. Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P07._Repeat_String
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int repeatTime = int.Parse(Console.ReadLine());
+
+            string repeatWord = RepeatWord(word, repeatTime);
+
+            Console.WriteLine(repeatWord);
+
+        }
+
+        static string RepeatWord(string word, int repeatTime)
+        {
+            string result = string.Empty;
+            for (int i = 1; i <= repeatTime; i++)
+            {
+                result += word;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ Methods/Lab/P08. Math Power/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P08._Math_Power
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            GetPower(@base, power);
+        }
+
+        static void GetPower(double @base, double power)
+        {
+            double result = Math.Pow(@base, power);
+            Console.WriteLine(result);
+
+        }
+    }
+}
