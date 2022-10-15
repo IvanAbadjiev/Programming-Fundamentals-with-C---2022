87472eff8a53eaba13e81a75f003de2e9ac4645e
IvanAbadjiev <ivan087611@gmail.com>
2022-10-15 22:30:44 +0300

    Methods
    • Declaring and Invoking Methods;
    • Value vs Reference Types;
    • Overloading Methods;
    • Naming and Best Practices.

---
+++ Methods/Lab/P05. Orders/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace P05._Orders
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //read inpput 
+            string order = Console.ReadLine(); //represent product coffee, water, coke, snacks
+            int quantity = int.Parse(Console.ReadLine());
+
+            PrintResult(order, quantity);
+
+
+        }
+        static double CalculatesPrice(string product, int qty)
+        {
+            double total = 0.00;
+            double productPrice = 0.00;
+            switch (product)
+            {
+                case "coffee":
+                    productPrice = 1.50;
+                    total = qty * productPrice;
+                    break;
+                case "water":
+                    productPrice = 1.00;
+                    total = qty * productPrice;
+                    break;
+
+                case "coke":
+                    productPrice = 1.40;
+                    total = qty * productPrice;
+                    break;
+
+                case "snacks":
+                    productPrice = 2.00;
+                    total = qty * productPrice;
+                    break;
+
+                default:
+                    break;
+            }
+            return total;
+
+        }
+
+        static void PrintResult(string product, int qty)
+        {
+            double result = CalculatesPrice(product, qty);
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
---
+++ Methods/Lab/P06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P06._Calculate_Rectangle_Area
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+
+            double area = GetRectangleArea(w, h);
+            Console.WriteLine(area);
+
+
+        }
+
+        static double GetRectangleArea(int w, int h)
+        {
+            double result = w * h;
+            return result;
+
+        }
+    }
+}
