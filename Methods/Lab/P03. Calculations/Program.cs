84e61828d2670a6a8d3850ed1b95a8ef47a1033f
IvanAbadjiev <ivan087611@gmail.com>
2022-10-15 22:29:47 +0300

    Methods
    • Declaring and Invoking Methods;
    • Value vs Reference Types;
    • Overloading Methods;
    • Naming and Best Practices.

---
+++ Methods/Lab/P03. Calculations/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace P03._Calculations
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //line of input 
+            string command = Console.ReadLine(); //add, multiply, subtract, divide
+            int a = int.Parse(Console.ReadLine()); //firstNumber
+            int b = int.Parse(Console.ReadLine()); //secondNumber
+
+            switch (command)
+            {
+                case "add":
+                    Add(a, b);
+                    break;
+                case "multiply":
+                    Multiply(a, b);
+                    break;
+                case "subtract":
+                    Subtract(a, b);
+                    break;
+                case "divide":
+                    Divide(a, b);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        static void Subtract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        static void Divide(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+
+    }
+}
---
+++ Methods/Lab/P04. Printing Triangle/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace P04._Printing_Triangle
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthTriangle = int.Parse(Console.ReadLine());
+
+            PrintFirstLine(lengthTriangle);
+            PrintSecondLine(lengthTriangle);
+
+
+        }
+
+        static void PrintFirstLine(int l)
+        {
+            for (int i = 1; i <= l; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(k + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintSecondLine(int l)
+        {
+
+            for (int i = l; i > 1; i--)
+            {
+                for (int k = 1; k < i; k++)
+                {
+                    Console.Write(k + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
+
