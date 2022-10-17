48b2491c52a707a694acee1c00a8a62b0fb5b107
IvanAbadjiev <ivan087611@gmail.com>
2022-10-17 22:16:15 +0300

    Exercise

---
+++ Methods/Exercise/P07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P07._NxN_Matrix
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNxnMatrix(n);
+        }
+
+        static void PrintNxnMatrix(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 2; k <= n; k++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
---
+++ Methods/Exercise/P08. Factorial Division/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P08._Factorial_Division
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+
+            float fact1 = CalculateFactorialOneNumber(one);
+            float fact2 = CalculateFactorialTwoNumber(two);
+
+            Console.WriteLine($"{fact1 / fact2:f2}");
+        }
+
+        static float CalculateFactorialOneNumber(int one)
+        {
+            float fact = 1;
+            for (int i = 1; i <= one; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+
+
+        }
+        static float CalculateFactorialTwoNumber(int two)
+        {
+            float fact = 1;
+            for (int i = 1; i <= two; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+
+
+        }
+
+    }
+}
