5041075dcb58e6a6bb3856541a9e3e3c6e2c6922
IvanAbadjiev <ivan087611@gmail.com>
2022-10-17 22:17:02 +0300

    Exercise

---
+++ Methods/Exercise/P09. Palindrome Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P09._Palindrome_Integers
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            CheksIntigerIsPalindrome(input);
+
+        }
+
+        static void CheksIntigerIsPalindrome(string input)
+        {
+
+            while (input != "END")
+            {
+                string reverseInput = string.Empty;
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reverseInput += input[i];
+                }
+                string normalNumber = input;
+                string reverseNumber = reverseInput;
+
+                if (normalNumber == reverseNumber)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                input = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ Methods/Exercise/P10. Top Number/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace P10._Top_Number
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            CheckNumberIsTopInt(n);
+        }
+
+        static void CheckNumberIsTopInt(int n)
+        {
+
+            for (int i = 1; i <= n; i++)
+            {
+                int copyI = i;
+                bool isDivide8 = false;
+                bool isOddNumber = false;
+                int sum = 0;
+                while (copyI != 0)
+                {
+                    int lastDigit = copyI % 10;
+                    sum += lastDigit;
+                    if (lastDigit % 2 != 0)
+                    {
+                        isOddNumber = true;
+                    }
+                    copyI /= 10;
+                }
+                if (sum % 8 == 0)
+                {
+                    isDivide8 = true;
+                }
+
+
+                if (isDivide8 && isOddNumber)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+
+
+    }
+}
