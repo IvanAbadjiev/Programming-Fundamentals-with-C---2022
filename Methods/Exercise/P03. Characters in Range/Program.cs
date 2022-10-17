76f3fe6f5966ac086159531bac97355b81843802
IvanAbadjiev <ivan087611@gmail.com>
2022-10-17 22:14:04 +0300

    Exercise

---
+++ Methods/Exercise/P03. Characters in Range/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P03._Characters_in_Range
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+
+            PrintCharInRange(one, two);
+        }
+
+        static void PrintCharInRange(char start, char end)
+        {
+
+            if (end < start)
+            {
+                int change = end;
+                end = start;
+                start = (char)change;
+            }
+            for (int i = start + 1; i < end; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+
+        }
+    }
+}
---
+++ Methods/Exercise/P04. Password Validator/Program.cs
@@ -0,0 +1,106 @@
+﻿
+namespace P04._Password_Validator
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            ValidatePassword(password);
+
+        }
+
+        static bool ChecksToConteainSixToThenCharecters(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool ChecksToAlphanumeric(string password)
+        {
+            bool isValid = true;
+            foreach (char item in password)
+            {
+                if (char.IsLetterOrDigit(item))
+                {
+
+                }
+                else
+                {
+                    isValid = false;
+                }
+
+            }
+            if (isValid)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
+
+
+
+        }
+
+        static bool ChecksToConteaTwoDigit(string password)
+        {
+            int digitCouter = 0;
+            foreach (char item in password)
+            {
+                if (Char.IsDigit(item))
+                {
+                    digitCouter++;
+                }
+            }
+            if (digitCouter >= 2)
+            {
+                return true;
+            }
+            else
+            {
+                {
+                    return false;
+                }
+            }
+        }
+
+        static void ValidatePassword(string password)
+        {
+            if (!ChecksToConteainSixToThenCharecters(password))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+
+
+            if (!ChecksToAlphanumeric(password))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+
+            if (!ChecksToConteaTwoDigit(password))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+
+            if (ChecksToConteainSixToThenCharecters(password) && ChecksToAlphanumeric(password) && ChecksToConteaTwoDigit(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+        }
+    }
+}
