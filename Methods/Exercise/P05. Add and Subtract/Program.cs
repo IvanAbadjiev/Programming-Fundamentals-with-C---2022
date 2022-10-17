c53f4ba562c8a661f00b6c7628c28df65fac7de0
IvanAbadjiev <ivan087611@gmail.com>
2022-10-17 22:15:13 +0300

    Exercise

---
+++ Methods/Exercise/P05. Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int afterAdd = Add(first, second);
+            int afterSubtract = Subtract(afterAdd, third);
+
+            Console.WriteLine(afterSubtract);
+        }
+
+        static int Add(int one, int two)
+        {
+            return one + two;
+        }
+
+        static int Subtract(int aferAdd, int three)
+        {
+            return aferAdd - three;
+        }
+    }
+}
---
+++ Methods/Exercise/P06. Middle Characters/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P06._Middle_Characters
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            PrintTheMidleChar(word);
+        }
+
+        static void PrintTheMidleChar(string word)
+        {
+            if (word.Length % 2 != 0)
+            {
+                char lether = word[word.Length / 2];
+                Console.WriteLine(lether);
+            }
+            else
+            {
+                char lether = word[word.Length / 2];
+                char lether2 = word[word.Length / 2 - 1];
+                Console.Write(lether2);
+                Console.Write(lether);
+            }
+
+        }
+    }
+}
