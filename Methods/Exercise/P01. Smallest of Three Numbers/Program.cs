fe179e578052f1c1a6af02ee78f8354a0ffa6be1
IvanAbadjiev <ivan087611@gmail.com>
2022-10-17 22:13:20 +0300

    Exercise

---
+++ Methods/Exercise/P01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P01._Smallest_of_Three_Numbers
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+
+            int smallestNUmber = GetSmallestNumber(first, second, third);
+
+            Console.WriteLine(smallestNUmber);
+
+        }
+
+        static int GetSmallestNumber(int one, int two, int thre)
+        {
+            if (one <= two && one <= thre)
+            {
+                return one;
+            }
+            else if (two <= one && two <= thre)
+            {
+                return two;
+            }
+            else if (thre <= one && thre <= two)
+            {
+                return thre;
+            }
+
+            return 0;
+
+        }
+    }
+}
---
+++ Methods/Exercise/P02. Vowels Count/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P02._Vowels_Count
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            int vowels = GetVowelsLetars(word);
+
+            Console.WriteLine(vowels);
+
+        }
+
+        static int GetVowelsLetars(string word)
+        {
+            int vowelsCount = 0;
+            char[] vowelsLether = new char[]
+            {
+                'a',
+                'e',
+                'i',
+                'o',
+                'u',
+            };
+
+            foreach (char item in word.ToLower())
+            {
+                if (vowelsLether.Contains(item))
+                {
+                    vowelsCount++;
+                }
+            }
+            return vowelsCount;
+
+
+        }
+    }
+}
