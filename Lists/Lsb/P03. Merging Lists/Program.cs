0ea8978c00b5a71d5291e607d5fae1f316f610ac
IvanAbadjiev <ivan087611@gmail.com>
2022-10-18 18:20:42 +0300

    List
    • What are Lists?;
    • Reading Lists from the Console;
    • Sorting Lists and Arrays.

---
+++ Lists/Lsb/P03. Merging Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace P03._Merging_Lists
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> second = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToList();
+
+            List<int> merging = new List<int>();
+
+            int smallList = Math.Min(first.Count, second.Count);
+            for (int i = 0; i < smallList; i++)
+            {
+                merging.Add(first[i]);
+                merging.Add(second[i]);
+            }
+
+            int bigList = Math.Max(first.Count, second.Count);
+            for (int i = smallList; i < bigList; i++)
+            {
+                if (first.Count > second.Count)
+                {
+                    merging.Add(first[i]);
+                }
+                else if (second.Count > first.Count)
+                {
+                    merging.Add(second[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", merging));
+
+
+        }
+    }
+}
---
+++ Lists/Lsb/P04. List of Products/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P04._List_of_Products
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products.Sort();
+            for (int i = 1; i <= products.Count; i++)
+            {
+                Console.WriteLine($"{i}.{products[i - 1]}");
+            }
+        }
+    }
+}
