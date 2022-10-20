07a32b318a21bea0c60e5ed6ae075fa38db57db6
IvanAbadjiev <ivan087611@gmail.com>
2022-10-20 22:38:45 +0300

    Exercise

---
+++ Lists/Exercise/P01. Train/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace P01._Train
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse)
+                   .ToList(); //integer represents the number of passengers 
+
+            int maxCapacoty = int.Parse(Console.ReadLine());
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] comArgs = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+                if (comArgs[0] == "Add")
+                {
+                    int newWagondPeople = int.Parse(comArgs[1]);
+                    wagons.Add(newWagondPeople);
+                    continue;
+                }
+
+                for (int i = 0; i < wagons.Count; i++)
+                {
+                    int diff = maxCapacoty - wagons[i];
+                    int peopleToIncom = int.Parse(comArgs[0]);
+                    if (diff >= peopleToIncom)
+                    {
+                        wagons[i] = wagons[i] + peopleToIncom;
+                        break;
+                    }
+                }
+
+
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+
+
+
+
+        }
+    }
+}
---
+++ Lists/Exercise/P02. Change List/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P02._Change_List
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string move = cmdArgs[0];
+                int element = int.Parse(cmdArgs[1]);
+
+                if (move == "Delete")
+                {
+                    numbers.RemoveAll(x => x == element);
+                }
+                else if (move == "Insert")
+                {
+                    int index = int.Parse(cmdArgs[2]);
+                    numbers.Insert(index, element);
+                }
+
+
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
