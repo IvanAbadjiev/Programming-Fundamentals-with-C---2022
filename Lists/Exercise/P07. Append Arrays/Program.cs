8201dbef173b82c13bc0d8548164ffdfcecc458a
IvanAbadjiev <ivan087611@gmail.com>
2022-10-20 22:41:26 +0300

    Exercise

---
+++ Lists/Exercise/P07. Append Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P07._Append_Arrays
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> number = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToList();
+
+            List<int> output = new List<int>();
+            foreach (var str in number)
+            {
+                output.AddRange(str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToList()
+                                     );
+            }
+            Console.WriteLine(string.Join(" ", output));
+
+        }
+    }
+}
