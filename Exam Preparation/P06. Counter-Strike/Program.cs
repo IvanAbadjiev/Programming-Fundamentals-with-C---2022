41f4b8d880ff5f686dd4fd7122b01c4435b7bf19
IvanAbadjiev <ivan087611@gmail.com>
2022-10-22 17:38:10 +0300

    Add new task

---
+++ Exam Preparation/P06. Counter-Strike/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P06._Counter_Strike
+{
+using System;
+    using System.Drawing;
+    using System.Text;
+    using System.Threading.Tasks.Dataflow;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int energy = int.Parse(Console.ReadLine());
+
+            string command;
+            int batlleCount = 0;    
+            while ((command = Console.ReadLine()) != "End of battle")
+            {
+                int costEnergi = int.Parse(command);
+                if (energy < 0)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {batlleCount} won battles and {energy} energy");
+                    Environment.Exit(0);
+                }
+               
+                    energy -= costEnergi;
+                    batlleCount++;
+                
+                if (batlleCount % 3 == 0)
+                {
+                    energy += batlleCount;
+                }
+            }
+
+            Console.WriteLine($"Won battles: {batlleCount}. Energy left: {energy}");
+
+        }
+    }
+}
