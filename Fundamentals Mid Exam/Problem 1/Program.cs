4af0d4f3f469042006a75195f1fa41b82cfa5207
IvanAbadjiev <ivan087611@gmail.com>
2022-10-23 16:00:17 +0300

    Mid Exam

---
+++ Fundamentals Mid Exam/Problem 1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem_1
+{
+using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            decimal flourPrice = decimal.Parse(Console.ReadLine());
+            decimal singleEggPrice = decimal.Parse(Console.ReadLine());
+            decimal sigleAprons = decimal.Parse(Console.ReadLine());
+
+            decimal studentMoreAprons = Math.Ceiling(students * 0.20m + students);
+            
+
+            decimal allApronsPrice = sigleAprons * studentMoreAprons;
+            decimal allEggPrce = (singleEggPrice * 10) * students;
+
+            int flourDiscount = students -  (students / 5);
+
+            decimal allFlourPrice = flourDiscount * flourPrice;
+
+
+            decimal allMoneyCost = allApronsPrice + allEggPrce + allFlourPrice;
+
+            if (allMoneyCost <= budget)
+            {
+                Console.WriteLine($"Items purchased for {allMoneyCost:f2}$.");
+            }
+            else
+            {
+                decimal diff = Math.Abs(budget - allMoneyCost);
+                Console.WriteLine($"{diff:f2}$ more needed.");
+            }
+
+
+        }
+    }
+}
