eaee08e2f3585e06a3004d98515a063513240550
IvanAbadjiev <ivan087611@gmail.com>
2022-10-27 13:52:28 +0300

    Objects and Classes
    • Objects;
    • Classes;
    • Built-in Classes;
    • Defining Simple Classes
    Classes.

---
+++ Objects and Classes/Lab/P02. Big Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P02._Big_Factorial
+{
+using System;
+    using System.Numerics;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            BigInteger fact = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
