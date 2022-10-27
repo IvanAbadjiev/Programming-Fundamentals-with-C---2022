f1cfd32ae94f2198986bf1eff890404594c7287d
IvanAbadjiev <ivan087611@gmail.com>
2022-10-27 13:51:47 +0300

    Objects and Classes
    • Objects;
    • Classes;
    • Built-in Classes;
    • Defining Simple Classes

---
+++ Objects and Classes/Lab/P01. Randomize Words/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P01._Randomize_Words
+{
+using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] word = Console.ReadLine()
+              .Split()
+              .ToArray();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                int rndNum = rnd.Next(0, word.Length);
+                string currentWord = word[i];
+                string randomWord = word[rndNum];
+                word[i] = randomWord;
+                word[rndNum] = currentWord;
+
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, word));
+
+
+
+
+        }
+    }
+}
