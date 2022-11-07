24508ea9cb996e29b765928dcc8b12c99d46f196
IvanAbadjiev <ivan087611@gmail.com>
2022-11-07 17:24:33 +0200

    Associative Arrays
    • Associative Arrays;
    • Lambda Expressions;
    • LINQ (Filtering, Mapping).

---
+++ Associative Arrays/Lab/P01. Count Real Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P01._Count_Real_Numbers
+{
+using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> keyValuePairs = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!keyValuePairs.ContainsKey(numbers[i]))
+                {
+                    keyValuePairs[numbers[i]] = 0;
+                }
+
+                keyValuePairs[numbers[i]]++;
+            }
+
+            foreach (var item in keyValuePairs)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
+
