be8322c62cb92973ad06b0c46f51c8f7ba0b84d1
IvanAbadjiev <ivan087611@gmail.com>
2022-11-07 17:25:57 +0200

    Associative Arrays
    • Associative Arrays;
    • Lambda Expressions;
    • LINQ (Filtering, Mapping).

---
+++ Associative Arrays/Lab/P03. Word Synonyms/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P03._Word_Synonyms
+{
+using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> wordAndSynonyms = new Dictionary<string, List<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonyms = Console.ReadLine();
+
+                if (!wordAndSynonyms.ContainsKey(word))
+                {
+                    wordAndSynonyms.Add(word, new List<string>());
+                    
+                }
+
+                    wordAndSynonyms[word].Add(synonyms);
+                
+                
+             
+
+            }
+
+            foreach (var item in wordAndSynonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ",item.Value)}");
+                
+            }
+
+        }
+    }
+}
