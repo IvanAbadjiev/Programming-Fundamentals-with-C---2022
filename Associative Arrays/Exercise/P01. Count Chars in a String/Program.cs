d6f6b2d9b361a5b2543c0918282d9ff8b0333ea6
IvanAbadjiev <ivan087611@gmail.com>
2022-11-08 18:41:23 +0200

    Exercise

---
+++ Associative Arrays/Exercise/P01. Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P01._Count_Chars_in_a_String
+{
+using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+            foreach (var ch in text)
+            {
+                if (ch != ' ')
+                {
+                    if (!keyValuePairs.ContainsKey(ch))
+                    {
+                        keyValuePairs[ch] = 0;
+                    }
+                    keyValuePairs[ch] += 1;
+
+                }
+
+            }
+
+            foreach (var kvp in keyValuePairs)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
