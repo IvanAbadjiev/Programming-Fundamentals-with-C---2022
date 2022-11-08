6b7ff519a6a72221a5bac6e49f0815a4a2c02f01
IvanAbadjiev <ivan087611@gmail.com>
2022-11-08 18:43:01 +0200

    Exercise

---
+++ Associative Arrays/Exercise/P04. SoftUni Parking/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace P04._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> register = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = cmdArgs[0];
+                string username = cmdArgs[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = cmdArgs[2];
+                    if (!register.ContainsKey(username))
+                    {
+                        register[username] = licensePlateNumber;
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+
+                }
+                else if (command == "unregister")
+                {
+                    if (!register.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user { username} not found");
+                    }
+                    else
+                    {
+                        register.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+
+
+
+                }
+
+            }
+
+            foreach (var kvp in register)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
+        }
+    }
+}
