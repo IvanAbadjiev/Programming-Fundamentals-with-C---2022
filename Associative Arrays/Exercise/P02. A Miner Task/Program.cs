6cc508d94e5daf090a7665e9e8eebe2417e1fc00
IvanAbadjiev <ivan087611@gmail.com>
2022-11-08 18:41:59 +0200

    Exercise

---
+++ Associative Arrays/Exercise/P02. A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P02._A_Miner_Task
+{
+using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resurce;
+
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            while ((resurce = Console.ReadLine()) != "stop")
+            {
+                int qty = int.Parse(Console.ReadLine());
+
+                if (!keyValuePairs.ContainsKey(resurce))
+                {
+                    keyValuePairs[resurce] = 0;
+                }
+                keyValuePairs[resurce] += qty;
+            }
+
+            foreach (var kvp in keyValuePairs)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+
+            }
+        }
+
+    }
+}
