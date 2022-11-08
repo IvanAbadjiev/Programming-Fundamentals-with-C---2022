ba6f0b5f4a08ee94c5c61f24b96dbc12ce4f5eac
IvanAbadjiev <ivan087611@gmail.com>
2022-11-08 18:44:16 +0200

    Exercise

---
+++ Associative Arrays/Exercise/P06. Student Academy/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace P06._Student_Academy
+{
+using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double value = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students[studentName] = new List<double>();
+                }
+                students[studentName].Add(value);
+            }
+
+            foreach (var kvp in students)
+            {
+                string studentsName = kvp.Key;
+                List<double> grade = kvp.Value;
+                double avrgGrade = 0;
+                foreach (var grd in grade)
+                {
+                    avrgGrade += grd;
+                }
+                avrgGrade /= grade.Count;
+
+                if (avrgGrade >= 4.50)
+                {
+                    Console.WriteLine($"{studentsName} -> {avrgGrade:f2}");
+                }
+            }
+
+        }
+    }
+}
