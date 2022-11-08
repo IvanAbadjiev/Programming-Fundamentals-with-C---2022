8808ceec0f6b7e607f8a83d7c3bad1ffa86a759f
IvanAbadjiev <ivan087611@gmail.com>
2022-11-08 18:44:43 +0200

    Exercise

---
+++ Associative Arrays/Exercise/P07. Company Users/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace P07._Company_Users
+{
+using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string command;
+
+            Dictionary<string, List<string>> register = new Dictionary<string, List<string>>();
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = cmdArgs[0];
+                string id = cmdArgs[1];
+
+                if (!register.ContainsKey(companyName))
+                {
+                    register[companyName] = new List<string>();
+                }
+               
+                    if (!register[companyName].Any(x => x == id))
+                    {
+                        register[companyName].Add(id);
+                    }
+                
+
+               
+
+            }
+
+            foreach (var kvp in register)
+            {
+                string companyName = kvp.Key;
+                List<string> id = kvp.Value;
+
+                Console.WriteLine(companyName);
+                foreach (var item in id)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+
+            }
+
+        }
+    }
+}
