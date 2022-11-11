1613683988cb7693fd9aae0754ce190c9b8ebc0b
IvanAbadjiev <ivan087611@gmail.com>
2022-11-11 17:28:35 +0200

    Text Processing
    • Strings;
    • Manipulating Strings;
    • Building and Modifying Strings.

---
+++ Text Processing/Lab/P04. Text Filter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banedWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+
+            foreach (var danWord in banedWords)
+            {
+                if (text.Contains(danWord))
+                {
+                    text = text.Replace(danWord, new string('*', danWord.Length));
+                }
+
+            }
+            Console.WriteLine(text);
+
+
+        }
+    }
+}
