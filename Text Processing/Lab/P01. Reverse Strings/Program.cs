3cc080ac7fe3832fef26835d7bf2c581d8e72a19
IvanAbadjiev <ivan087611@gmail.com>
2022-11-11 17:26:22 +0200

    Text Processing
    • Strings;
    • Manipulating Strings;
    • Building and Modifying Strings.

---
+++ Text Processing/Lab/P01. Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P01._Reverse_Strings
+{
+using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                string reverseWord = string.Empty;
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reverseWord += word[i];
+                }
+                Console.WriteLine($"{word} = {reverseWord}");
+            }
+
+        }
+    }
+}
