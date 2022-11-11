d2ba7536bc773d8c46a3891da5d622ca67bf61dc
IvanAbadjiev <ivan087611@gmail.com>
2022-11-11 17:27:04 +0200

    Text Processing
    • Strings;
    • Manipulating Strings;
    • Building and Modifying Strings.

---
+++ Text Processing/Lab/P02. Repeat Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace P02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                int repeatTime = word.Length;
+                for (int i = 0; i < repeatTime; i++)
+                {
+                    result.Append(word);
+                }
+
+
+            }
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
