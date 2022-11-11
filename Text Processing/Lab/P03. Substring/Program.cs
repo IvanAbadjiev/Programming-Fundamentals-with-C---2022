00be8aac972885d129dcc1a439bf1fcd8d6cdeb6
IvanAbadjiev <ivan087611@gmail.com>
2022-11-11 17:27:38 +0200

    Text Processing
    • Strings;
    • Manipulating Strings;
    • Building and Modifying Strings.

---
+++ Text Processing/Lab/P03. Substring/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P03._Substring
+{
+using System;
+    using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder digit = new StringBuilder();
+            StringBuilder later = new StringBuilder();
+            StringBuilder other = new StringBuilder();
+
+            foreach (char item in text)
+            {
+                if (char.IsDigit(item))
+                {
+                    digit.Append(item);
+                }
+                else if (char.IsLetter(item))
+                {
+                    later.Append(item);
+                }
+                else
+                {
+                    other.Append(item);
+                }
+                
+                
+
+                
+
+            }
+            Console.WriteLine($"{digit}{Environment.NewLine}{later}{Environment.NewLine}{other}");
+
+
+        }
+    }
+}
