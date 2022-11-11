234fda47b8a5add0d85883d5b38e2772ace833da
IvanAbadjiev <ivan087611@gmail.com>
2022-11-11 17:29:03 +0200

    Text Processing
    • Strings;
    • Manipulating Strings;
    • Building and Modifying Strings.

---
+++ Text Processing/Lab/P05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P05._Digits__Letters_and_Other
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(word))
+            {
+                int startIndex = text.IndexOf(word);
+               text = text.Remove(startIndex, word.Length);
+
+
+            }
+            Console.WriteLine(text);
+
+        }
+    }
+}
