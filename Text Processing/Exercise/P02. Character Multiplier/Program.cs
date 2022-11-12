127155d0f8e33f1e396490840c5b025478963f52
IvanAbadjiev <ivan087611@gmail.com>
2022-11-12 20:52:52 +0200

    Exercise

---
+++ Text Processing/Exercise/P02. Character Multiplier/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P02._Character_Multiplier
+{
+using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int result = GetCharecterValue(input[0], input[1]);
+            Console.WriteLine(result);
+
+        }
+
+        private static int GetCharecterValue(string strOne, string strTwo)
+        {
+            int minLength = Math.Min(strOne.Length , strTwo.Length);
+            int sum = 0;
+            for (int i = 0; i < minLength; i++)
+            {
+                sum += strOne[i] * strTwo[i];
+            }
+
+            string bigestStr = strOne;
+
+            if (strTwo.Length > bigestStr.Length)
+            {
+                bigestStr = strTwo;
+            }
+
+            for (int i = minLength; i < bigestStr.Length; i++)
+            {
+                sum += bigestStr[i];
+            }
+
+            return sum;
+        }
+    }
+}
