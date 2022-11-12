869bf19a263a99592912a84028531181e551481e
IvanAbadjiev <ivan087611@gmail.com>
2022-11-12 20:53:56 +0200

    Exercise

---
+++ Text Processing/Exercise/P04. Caesar Cipher/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace P04._Caesar_Cipher
+{
+using System;
+    using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            var criptedText = new StringBuilder();
+            foreach (char ch in text)
+            {
+                
+                
+                    int value = ch + 3;
+                    char crip = (char)value;
+                    criptedText.Append(crip);
+                
+               
+            }
+           string result = criptedText.ToString();
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
