425fc872863f1f33d25bc9694cfa0505b7048b21
IvanAbadjiev <ivan087611@gmail.com>
2022-11-12 20:52:04 +0200

    Exercise

---
+++ Text Processing/Exercise/P01. Valid Usernames/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P01._Valid_Usernames
+{
+using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string username in usernames)
+            {
+                if (username.Length > 3 && username.Length <= 16)
+                {
+                        bool isValid = true;
+                    foreach (char ch in username)
+                    {
+                        if (!(char.IsLetterOrDigit(ch) || ch == '-' || ch == '_'))
+                        {
+                            isValid = false;
+                            break;
+                        }
+                        
+
+                    }
+                    if (isValid)
+                    {
+                        Console.WriteLine(username);
+                    }
+
+                }
+                
+
+            }
+
+
+        }
+    }
+}
