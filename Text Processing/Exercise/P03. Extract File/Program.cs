16cbcfeb6c5bea3b34218f43fd2f979004249053
IvanAbadjiev <ivan087611@gmail.com>
2022-11-12 20:53:21 +0200

    Exercise

---
+++ Text Processing/Exercise/P03. Extract File/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P03._Extract_File
+{
+using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filePath = Console.ReadLine()
+                .Split("\\", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] fileDate = filePath[filePath.Length - 1].Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine($"File name: {fileDate[0]}");
+            Console.WriteLine($"File extension: {fileDate[1]}");
+
+
+        }
+    }
+}
